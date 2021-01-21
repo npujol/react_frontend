4607d2ff76fe9aad0793f4d30c403636c520e053
npujol <naivy.luna@gmail.com>
2021-01-21 14:32:40 +0100

    initial state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { store, history} from './store';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+import App from './components/App/App';
+
+// import { ApiClient } from "./client";
+
+// const apiClient = ApiClient.instance;
+// if (process.env.NODE_ENV === "production") {
+//   apiClient.basePath = "/api";
+// } else {
+//   apiClient.basePath = "http://localhost:8000/api".replace(/\/+$/, "");
+// }
+
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+
+), document.getElementById('root'));
+
+
