5177cedb3d5a576a58cef0bab4cb1baf52b8fdec
npujol <naivy.luna@gmail.com>
2021-01-25 16:56:29 +0100

    Add thema

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
-import JwtService from "../../jwt.service";
-import Header from './Header';
-import React from 'react';
+
 import { connect } from 'react-redux';
 import { APP_LOAD, REDIRECT } from '../../constants/actionTypes';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import JwtService from "../../jwt.service";
+import Header from './Header';
 import Story from '../Story/Story';
 import Editor from '../Story/Editor';
 import Home from '../Home';
@@ -15,6 +15,13 @@ import Settings from '../Profile/Settings';
 import { store } from '../../store';
 import { push } from 'react-router-redux';
 import { UsersApi } from "../../client"
+
+
+import React, { Fragment, Suspense } from "react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "../../theme";
+import GlobalStyles from "../../GlobalStyles";
+// import * as serviceWorker from "./serviceWorker";
 
 const usersApi = new UsersApi();
 
@@ -51,28 +58,37 @@ class App extends React.Component {
     }
 
     this.props.onLoad(token ? usersApi.usersRead(JwtService.getUsername()) : null, token);
-    console.log("in middleware", this.props);
+    // console.log("in middleware", this.props);
   }
 
   render() {
     if (this.props.appLoaded) {
       return (
-        <div>
-          <Header
-            appName={this.props.appName}
-            currentUser={this.props.currentUser} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/editor/:slug" component={Editor} />
-            <Route path="/editor" component={Editor} />
-            <Route path="/story/:id" component={Story} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/@:username/favorites" component={ProfileFavorites} />
-            <Route path="/@:username" component={Profile} />
-          </Switch>
-        </div>
+        <BrowserRouter>
+          <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <GlobalStyles />
+            <div>
+              <Header
+                appName={this.props.appName}
+                currentUser={this.props.currentUser} />
+            </div>
+            <Suspense fallback={<Fragment />}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/editor/:slug" component={Editor} />
+                <Route path="/editor" component={Editor} />
+                <Route path="/story/:id" component={Story} />
+                <Route path="/settings" component={Settings} />
+                <Route path="/@:username/favorites" component={ProfileFavorites} />
+                <Route path="/@:username" component={Profile} />
+              </Switch>
+            </Suspense>
+          </MuiThemeProvider>
+        </BrowserRouter>
+
       );
     }
     return (
