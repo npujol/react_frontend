34a16c86b5f9ed3f4c8cccf5a8c694a7a478c10b
npujol <naivy.luna@gmail.com>
2021-02-12 14:59:18 +0100

    Update general index in home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,20 @@
-import Banner from './Banner';
-import MainView from './MainView';
-import React from 'react';
-import Tags from './Tags';
+import MainView from "./MainView";
+import React from "react";
+import Tags from "./Tags";
 import { TagsApi, StoriesApi } from "../../client";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
 import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
-  APPLY_TAG_FILTER
-} from '../../constants/actionTypes';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-
-
+  APPLY_TAG_FILTER,
+} from "../../constants/actionTypes";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
 
 const Promise = global.Promise;
 
@@ -27,20 +27,40 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
+  },
+  banner: {
+    padding: theme.spacing(2),
+    textAlign: "left",
+    color: theme.palette.text.secondary,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
   },
 }));
 
-const GeneralHomeGrid = props => {
+const GeneralHomeGrid = (props) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <Banner token={props.token} appName={props.appName} />
+          <Paper className={classes.banner}>
+            <Link to="/editor">
+              <Fab color="secondary" aria-label="add">
+                <AddIcon />
+              </Fab>
+            </Link>
+            <Typography
+              className={classes.paper}
+              variant="h4"
+              component="h2"
+              gutterBottom
+            >
+              A place to share your stories
+            </Typography>
           </Paper>
         </Grid>
         <Grid item xs={9}>
@@ -53,9 +73,7 @@ const GeneralHomeGrid = props => {
             <Typography variant="h5" component="h5" gutterBottom>
               Popular Tags
             </Typography>
-            <Tags
-              tags={props.tags}
-              onClickTag={props.onClickTag} />
+            <Tags tags={props.tags} onClickTag={props.onClickTag} />
           </Paper>
         </Grid>
       </Grid>
@@ -63,35 +81,34 @@ const GeneralHomeGrid = props => {
   );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   ...state.home,
   appName: state.common.appName,
-  token: state.common.token
+  token: state.common.token,
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   onClickTag: (tag, pager, payload) =>
     dispatch({ type: APPLY_TAG_FILTER, tag, pager, payload }),
   onLoad: (tab, pager, payload) =>
     dispatch({ type: HOME_PAGE_LOADED, tab, pager, payload }),
-  onUnload: () =>
-    dispatch({ type: HOME_PAGE_UNLOADED })
+  onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED }),
 });
 
 class Home extends React.Component {
   componentWillMount() {
-    const tab = this.props.token ? 'feed' : 'all';
-    const storiesPromise = this.props.token ?
-      storiesApi.storiesFeedList :
-      storiesApi.storiesList;
+    const tab = this.props.token ? "feed" : "all";
+    const storiesPromise = this.props.token
+      ? storiesApi.storiesFeedList
+      : storiesApi.storiesList;
     this.props.onLoad(
       tab,
       storiesPromise,
       Promise.all([
         tagsApi.tagsList(),
-        storiesApi.storiesFeedList({ offset: 0, limit: 10 })
-      ]));
-
+        storiesApi.storiesFeedList({ offset: 0, limit: 10 }),
+      ])
+    );
   }
 
   componentWillUnmount() {
@@ -108,7 +125,7 @@ class Home extends React.Component {
           tags={this.props.tags}
         />
       </div>
-    )
+    );
   }
 }
 
