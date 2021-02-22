00b3cb9ebab5f931f88e51fc0504e750fbbbf1fd
npujol <naivy.luna@gmail.com>
2021-02-22 17:57:37 +0100

    Remove the store middleware

--- src/store.js
+++ src/store.js
@@ -2,18 +2,13 @@ import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunkMiddleware from "redux-thunk";
-import { localStorageMiddleware } from "./middleware.js";
 import reducer from "./reducer";
 
 const getMiddleware = () => {
   if (process.env.NODE_ENV === "production") {
-    return applyMiddleware(localStorageMiddleware, thunkMiddleware);
+    return applyMiddleware(thunkMiddleware);
   } else {
-    return applyMiddleware(
-      localStorageMiddleware,
-      thunkMiddleware,
-      createLogger()
-    );
+    return applyMiddleware(thunkMiddleware, createLogger());
   }
 };
 
