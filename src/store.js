f1d1b1da9b796d49e645629e6c3682b0928a038a
npujol <naivy.luna@gmail.com>
2021-02-22 11:46:42 +0100

    Add thunkMiddleware

--- src/store.js
+++ src/store.js
@@ -1,20 +1,14 @@
 import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
-import { promiseMiddleware, localStorageMiddleware } from "./middleware";
+import thunkMiddleware from "redux-thunk";
 import reducer from "./reducer";
 
-// Build the middleware for intercepting and dispatching navigation actions
 const getMiddleware = () => {
   if (process.env.NODE_ENV === "production") {
-    return applyMiddleware(promiseMiddleware, localStorageMiddleware);
+    return applyMiddleware(thunkMiddleware);
   } else {
-    // Enable additional logging in non-production environments.
-    return applyMiddleware(
-      promiseMiddleware,
-      localStorageMiddleware,
-      createLogger()
-    );
+    return applyMiddleware(thunkMiddleware, createLogger());
   }
 };
 
