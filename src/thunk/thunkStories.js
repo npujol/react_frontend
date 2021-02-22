d236d4345f3ddfb21837715cadb6bbae0703e9e5
npujol <naivy.luna@gmail.com>
2021-02-22 17:26:15 +0100

    Add stories hunks

---
+++ src/thunk/thunkStories.js
@@ -0,0 +1,52 @@
+import { HOME_PAGE_LOADED } from "../constants/actionTypes.js";
+
+import { TagsApi, StoriesApi } from "../client";
+const storiesApi = new StoriesApi();
+const tagsApi = new TagsApi();
+
+export const fetchStoriesGlobal = () => {
+  return async (dispatch) => {
+    const tags = await tagsApi.tagsList();
+    const stories = await storiesApi.storiesFeedList({ offset: 0, limit: 10 });
+    const payload = [tags, stories];
+    dispatch({ type: HOME_PAGE_LOADED, payload });
+  };
+};
+export const fetchStoriesFavorites = (username) => {
+  return async (dispatch) => {
+    const tags = await tagsApi.tagsList();
+    const stories = await storiesApi.storiesList({
+      favoritedByUserUsername: username,
+      offset: 0,
+      limit: 10,
+    });
+    const payload = [tags, stories];
+    dispatch({ type: HOME_PAGE_LOADED, payload });
+  };
+};
+
+export const fetchStoriesTag = (tag) => {
+  return async (dispatch) => {
+    const tags = await tagsApi.tagsList();
+    const stories = await storiesApi.storiesList({
+      tagsTag: tag,
+      offset: 0,
+      limit: 10,
+    });
+    const payload = [tags, stories];
+    dispatch({ type: HOME_PAGE_LOADED, payload });
+  };
+};
+
+export const fetchStoriesYours = (username) => {
+  return async (dispatch) => {
+    const tags = await tagsApi.tagsList();
+    const stories = await storiesApi.storiesList({
+      ownerUserUsername: username,
+      offset: 0,
+      limit: 10,
+    });
+    const payload = [tags, stories];
+    dispatch({ type: HOME_PAGE_LOADED, payload });
+  };
+};
