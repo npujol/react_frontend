f5bc060f3ae951f3a30207edd1d1388b9a43fa35
npujol <naivy.luna@gmail.com>
2021-02-22 17:21:53 +0100

    Add thuk

--- src/store.js
+++ src/store.js
@@ -2,13 +2,18 @@ import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunkMiddleware from "redux-thunk";
+import { localStorageMiddleware } from "./middleware.js";
 import reducer from "./reducer";
 
 const getMiddleware = () => {
   if (process.env.NODE_ENV === "production") {
-    return applyMiddleware(thunkMiddleware);
+    return applyMiddleware(localStorageMiddleware, thunkMiddleware);
   } else {
-    return applyMiddleware(thunkMiddleware, createLogger());
+    return applyMiddleware(
+      localStorageMiddleware,
+      thunkMiddleware,
+      createLogger()
+    );
   }
 };
 
